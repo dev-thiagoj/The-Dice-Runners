076555542e6d0d01356848d4e70d027ebf527d80
Dev.ThiagoJ <dev.thiagoj@gmail.com>
2022-07-29 10:45:02 -0300

    fix pieces architeture

--- Assets/Scripts/LevelManager/LevelManager.cs
+++ Assets/Scripts/LevelManager/LevelManager.cs
@@ -7,14 +7,18 @@ public class LevelManager : MonoBehaviour
     public Transform levelContainer;
     public List<GameObject> levels;
 
+    [Header("Pieces")]
+    public List<GameObject> levelPieces;
+    public int numberOfPieces;
+
     [SerializeField] int _index;
     [SerializeField] GameObject _currLevel;
+    [SerializeField] List<GameObject> _spawnedPieces;
 
     private void Awake()
     {
-        SpawnNextLevel();
+        //SpawnNextLevel();
     }
-
     
     [NaughtyAttributes.Button]
     void SpawnNextLevel()
@@ -39,5 +43,26 @@ public class LevelManager : MonoBehaviour
         _index = 0;
     }
 
-    
+    void CreatePieces()
+    {
+        var piece = levelPieces[Random.Range(0, levelPieces.Count)];
+        var spawnedPiece = Instantiate(piece, levelContainer);
+
+        if(_spawnedPieces.Count > 0)
+        {
+            var lastPiece = _spawnedPieces[_spawnedPieces.Count - 1];
+        }
+
+        _spawnedPieces.Add(spawnedPiece);
+    }
+
+    void CreateLevel()
+    {
+        _spawnedPieces = new List<GameObject>();
+
+        for (int i = 0; i < numberOfPieces; i++)
+        {
+            CreatePieces();
+        }
+    }
 }
